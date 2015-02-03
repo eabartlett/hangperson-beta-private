fd2be215448b54b0862678b5afa88b99d043727e
Erik Bartlett <eabartlett@berkeley.edu>
2015-02-03 03:58:16 -0400

    Changes to HangpersonGame class

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,7 +1,50 @@
 class HangpersonGame
 
-  # add the necessary class methods, attributes, etc. here
-  # to make the tests in spec/hangperson_game_spec.rb pass.
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  attr_accessor :word
+  def initialize(new_word)
+    @word = new_word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+
+  def guess(letter)
+    letter.gsub!(/[^a-zA-z]/i, '')
+    if letter.size == 0 or not letter 
+      raise ArgumentError
+    end
+    if @guesses.include? letter or @wrong_guesses.include? letter
+      return false
+    end
+    if @word.include? letter
+      @guesses = @guesses << letter
+      return true
+    end
+    @wrong_guesses = @wrong_guesses << letter
+    return true
+  end
+
+  def word_with_guesses
+    word.chars.map do |l| 
+      if @guesses.include? l 
+        l
+      else
+        "-"
+      end
+    end.join
+  end
+
+  def check_win_or_lose
+    if @wrong_guesses.size >= 7
+      :lose
+    elsif not word_with_guesses.include? "-"
+      :win
+    else
+      :play
+    end
+  end
+
 
   # Get a word from remote "random word" service
 
